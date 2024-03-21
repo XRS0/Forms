a1430f440819bfd36517b83f3965ba7d64e1cfc0
Максим Пьянков <pya4kov@gmail.com>
2024-03-21 13:12:10 +0500

    realizated auth service, added jwt service

---
+++ client/client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/XRS0/Forms/services/auth/gen"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	c := pb.NewAuthServiceClient(conn)
+	resp, err := c.Login(context.Background(), &pb.LoginRequest{Username: "John", Password: "жопажопа"})
+	if err != nil {
+		log.Fatalf("could not login	: %v", err)
+	}
+	fmt.Println("Message:", resp.Message)
+}
---
+++ services/auth/server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/XRS0/Forms/services/auth/gen"
+	"google.golang.org/grpc"
+)
+
+type authServer struct {
+	pb.UnimplementedAuthServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterAuthServiceServer(s, &authServer{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func (s *authServer) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginResponse, error) {
+	fmt.Println("Received: чел с ником", in.Username, "и с паролем", in.Password)
+	if in.Username == "John" && in.Password == "жопажопа" {
+		return &pb.LoginResponse{Message: "туалет открыт"}, nil
+	}
+	return nil, grpc.Errorf(7, "вы не обладаете полномочиями какать здесь")
+}
