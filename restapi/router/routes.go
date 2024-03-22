d4580680de7f1fbf2770fec14a969271f84a2967
Максим Пьянков <pya4kov@gmail.com>
2024-03-22 09:15:36 +0500

    added connect middleware rest rpc

--- restapi/router/routes.go
+++ restapi/router/routes.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"context"
+	"log"
 	"net/http"
 
+	pbAuth "github.com/XRS0/Forms/services/auth/gen"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 // AuthCredentials структура для хранения данных аутентификации
@@ -32,9 +36,18 @@ func RouteAuth(r *gin.Engine) {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+		connAuth, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		cAuth := pbAuth.NewAuthServiceClient(connAuth)
+		respAuth, errAuth := cAuth.Login(context.Background(), &pbAuth.LoginRequest{Username: "John", Password: "жопажопа"})
+		if errAuth != nil {
+			log.Fatalf("could not login	: %v", err)
+		}
 
-		success := creds.Username == "sum" && creds.Password == "jwt"
+		// success := creds.Username == "sum" && creds.Password == "jwt"
 
-		c.JSON(http.StatusOK, gin.H{"success": success})
+		c.JSON(http.StatusOK, gin.H{"success": respAuth})
 	})
 }
